8a2144136e1d38e404eed2b5e68125aff239b9ea
panandafog <panandafog@gmail.com>
2022-06-21 18:31:23 +0300

    Initial Commit

---
+++ Accelerometer/AccelerometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccelerometerApp.swift
+//  Accelerometer
+//
+//  Created by Andrey on 21.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AccelerometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccelerometerUITests/AccelerometerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AccelerometerUITestsLaunchTests.swift
+//  AccelerometerUITests
+//
+//  Created by Andrey on 21.06.2022.
+//
+
+import XCTest
+
+class AccelerometerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
