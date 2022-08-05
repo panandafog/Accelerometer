848e529513b3294ff524438b67f7c8392ff9c2eb
panandafog <panandafog@gmail.com>
2022-08-05 13:18:52 +0300

    removed comments

--- Accelerometer/Recordings/Database/RecordingsRepository.swift
+++ Accelerometer/Recordings/Database/RecordingsRepository.swift
@@ -34,47 +34,4 @@ class RecordingsRepository: ObservableObject {
         }
         databaseManager.delete([recording])
     }
-    
-    //    var recordings: [Recording] {
-    //        recordingEntities.map { Recording(from: $0) }
-    //    }
-    
-    //    private var recordingEntities: [RecordingEntity] = []
-    //    private let persistenceManager: PersistenceManager
-    
-    //    init() {
-    //        persistenceManager = PersistenceManager()
-    //        persistenceManager.loadPersistentStores() { [ weak self ] in
-    //            self?.fetchSavedRecordings()
-    //        }
-    //    }
-    
-    //    private func fetchSavedRecordings() {
-    //        let request = RecordingEntity.fetchRequest()
-    //        let sort = NSSortDescriptor(key: "date", ascending: false)
-    //        request.sortDescriptors = [sort]
-    //
-    //        do {
-    //            recordingEntities = try persistenceManager.container.viewContext.fetch(request)
-    //            print("Got \(recordingEntities.count) recordings")
-    //        } catch {
-    //            print("Fetch failed")
-    //        }
-    //    }
-    
-    //    func save(recording newRecording: Recording) {
-    //        let newRecordingEntity = newRecording.entity
-    //        if let index = recordingEntities.firstIndex(where: { $0.id == newRecordingEntity.id }) {
-    //            recordingEntities[index] = newRecordingEntity
-    //        } else {
-    //            recordingEntities.insert(newRecordingEntity, at: 0)
-    //        }
-    //        persistenceManager.saveContext()
-    //    }
-    //
-    //    func delete(recordingID: String) {
-    //        recordings.removeAll { recording in
-    //            recording.id == recordingID
-    //        }
-    //    }
 }
