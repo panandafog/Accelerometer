a25db329185fb625b63689d8d9f99880ac6f10a8
Andrey Pantyuhin <panandafog@gmail.com>
2025-09-17 19:48:02 +0300

    Now recording chart starts from first entry datetime

--- Accelerometer/Recordings/RecordingMeasurementChartView.swift
+++ Accelerometer/Recordings/RecordingMeasurementChartView.swift
@@ -15,6 +15,10 @@ struct RecordingMeasurementChartView: View {
     
     let presentExporter: () -> Void
     
+    private var startDate: Date {
+        chartEntries.first?.date ?? Date()
+    }
+    
     var title: String { measurementType.name.capitalizingFirstLetter() }
     
     var chartEntries: [Recording.Entry] {
@@ -28,7 +32,7 @@ struct RecordingMeasurementChartView: View {
                 .padding(.top, 4)
             
             Chart(chartEntries) { entry in
-                CustomChartContent(entry: entry)
+                CustomChartContent(entry: entry, startDate: startDate)
             }
             .frame(height: 200)
             .padding(.horizontal, 16)
@@ -39,10 +43,10 @@ struct RecordingMeasurementChartView: View {
 
 private struct CustomChartContent: ChartContent {
     let entry: Recording.Entry
+    let startDate: Date
     
     var body: AnyChartContent {
         let axes = entry.axes
-        
         AnyChartContent(erasing: createLineMarks(axes: axes))
     }
     
@@ -53,12 +57,12 @@ private struct CustomChartContent: ChartContent {
         
         ForEach(axesTypes) { axeType in
             if let yAxis = axes.values[axeType] {
-                
-                let xName = "date"
+                let elapsed = entry.date.timeIntervalSince(startDate)
+                let xName = "elapsed"
                 let yName = yAxis.type_.name
                 
                 LineMark(
-                    x: .value(xName, entry.date),
+                    x: .value(xName, elapsed),
                     y: .value(yName, yAxis.value)
                 )
                 .foregroundStyle(by: .value("name", yName))
@@ -69,84 +73,116 @@ private struct CustomChartContent: ChartContent {
 
 struct RecordingMeasurementChartView_Previews: PreviewProvider {
     
-    static let axes1: TriangleAxes = {
+    static var axesTemplate: TriangleAxes {
         var axes = TriangleAxes.zero
+        let xValue = Double.random(in: -1.0...1.0)
+        let yValue = Double.random(in: -1.0...1.0)
+        let zValue = Double.random(in: -1.0...1.0)
+
         axes.displayableAbsMax = 1.0
         axes.values = [
-            .x: Axis(type_: .x, value: 0.0),
-            .y: Axis(type_: .y, value: 0.0),
-            .z: Axis(type_: .z, value: 0.0)
+            .x: Axis(type_: .x, value: xValue),
+            .y: Axis(type_: .y, value: yValue),
+            .z: Axis(type_: .z, value: zValue)
         ]
         return axes
-    }()
+    }
     
-    static let axes2: TriangleAxes = {
-        var axes = TriangleAxes.zero
-        axes.displayableAbsMax = 1.0
-        axes.values = [
-            .x: Axis(type_: .x, value: 1.0),
-            .y: Axis(type_: .y, value: 0.1),
-            .z: Axis(type_: .z, value: 0.1)
-        ]
-        return axes
-    }()
-    
-    static let axes3: TriangleAxes = {
-        var axes = TriangleAxes.zero
-        axes.displayableAbsMax = 1.0
-        axes.values = [
-            .x: Axis(type_: .x, value: 0.2),
-            .y: Axis(type_: .y, value: 0.2),
-            .z: Axis(type_: .z, value: 0.2)
-        ]
-        return axes
-    }()
-    
-    static let baseAxes: [TriangleAxes] = [axes1, axes2, axes3]
-    
-    static var allEntries: [Recording.Entry] {
-        let totalPoints = 50
-        let pointsPerSecond = 1.0
+    static var secondsEntries: [Recording.Entry] {
+        let totalPoints = 60
+        let pointsPerSecond = 30.0
         let startDate = Date()
         
         return (0..<totalPoints).map { index in
             let timeOffset = Double(index) / pointsPerSecond
             let date = startDate.addingTimeInterval(timeOffset)
             
-            let xValue = Double.random(in: -1.0...1.0)
-            let yValue = Double.random(in: -1.0...1.0)
-            let zValue = Double.random(in: -1.0...1.0)
-            
-            var axes = TriangleAxes.zero
-            axes.displayableAbsMax = 1.0
-            axes.values = [
-                .x: Axis(type_: .x, value: xValue),
-                .y: Axis(type_: .y, value: yValue),
-                .z: Axis(type_: .z, value: zValue)
-            ]
-            
             return Recording.Entry(
                 measurementType: .acceleration,
                 date: date,
-                axes: axes
+                axes: axesTemplate
+            )
+        }
+    }
+    
+    static var minutesEntries: [Recording.Entry] {
+        let totalPoints = 20 * 60
+        let samplesPerSecond = 1.0
+        let start = Date()
+        return (0..<totalPoints).map { i in
+            let t = Double(i) / samplesPerSecond
+            let date = start.addingTimeInterval(t)
+            return .init(
+                measurementType: .acceleration,
+                date: date,
+                axes: axesTemplate
+            )
+        }
+    }
+    
+    static var hoursEntries: [Recording.Entry] {
+        let totalPoints = (2 * 60 * 60) / 5
+        let interval = 5.0
+        let start = Date()
+        return (0..<totalPoints).map { i in
+            let t = Double(i) * interval
+            let date = start.addingTimeInterval(t)
+            return .init(
+                measurementType: .acceleration,
+                date: date,
+                axes: axesTemplate
             )
         }
     }
 
     
     static var previews: some View {
-        RecordingMeasurementChartView(
-            recording: Recording(
-                entries: allEntries,
-                state: .completed,
-                measurementTypes: [.acceleration]
-            ),
-            measurementType: .acceleration,
-            presentExporter: {}
-        )
+        VStack {
+            Text("2 sec")
+                .padding(.bottom, 40)
+            RecordingMeasurementChartView(
+                recording: Recording(
+                    entries: secondsEntries,
+                    state: .completed,
+                    measurementTypes: [.acceleration]
+                ),
+                measurementType: .acceleration,
+                presentExporter: {}
+            )
+            .frame(width: 300, height: 150)
+            .background(Color(.systemBackground))
+            .padding(.bottom, 40)
+            
+            Text("20 min")
+                .padding(.bottom, 40)
+            RecordingMeasurementChartView(
+                recording: Recording(
+                    entries: minutesEntries,
+                    state: .completed,
+                    measurementTypes: [.acceleration]
+                ),
+                measurementType: .acceleration,
+                presentExporter: {}
+            )
+            .frame(width: 300, height: 150)
+            .background(Color(.systemBackground))
+            .padding(.bottom, 40)
+            
+            Text("2 h")
+                .padding(.bottom, 40)
+            RecordingMeasurementChartView(
+                recording: Recording(
+                    entries: hoursEntries,
+                    state: .completed,
+                    measurementTypes: [.acceleration]
+                ),
+                measurementType: .acceleration,
+                presentExporter: {}
+            )
+            .frame(width: 300, height: 150)
+            .background(Color(.systemBackground))
+            .padding(.bottom, 40)
+        }
         .previewLayout(.sizeThatFits)
-        .frame(width: 300, height: 150)
-        .background(Color(.systemBackground))
-        .padding()
     }
 }
